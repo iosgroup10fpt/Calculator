5568e3581570fe4cc1917570e0d393e5194a5937
ThanhKhoa <tthanhkhoa@gmail.com>
2017-04-20 17:40:49 +0700

    no message

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -35,21 +35,76 @@ class ViewController: UIViewController {
     @IBOutlet weak var lb_display: UILabel!
     //Event bam so
     @IBAction func numbers(_ sender: UIButton) {
-        if performingMath == true {
-            previousnumber = Double(lb_display.text!)!
-            lb_display.text = String(sender.tag - 1)
-            performingMath = false
+        if(afterequal){
+            lb_display.text = String(0)
         }
-        else {
-            lb_display.text = lb_display.text! + String(sender.tag - 1)
-            numberScreen = Double(lb_display.text!)!
-            
+        else{
+            if performingMath == true {
+                previousnumber = Double(lb_display.text!)!
+                
+                lb_display.text = String(sender.tag - 1)
+                //NSLog(String(sender.tag - 1))
+                performingMath = false
+            }
+            else {
+                do{
+                    
+                    if(lb_display.text == "0"){
+                        lb_display.text = ""
+                        lb_display.text = lb_display.text! + String(sender.tag - 1)
+                        numberScreen = Double(lb_display.text!)!
+                    }
+                    else if(lb_display.text == "inf"){
+                        lb_display.text = String(0)
+                        //                numberScreen = Double(lb_display.text!)!
+                    }
+                    else if(lb_display.text == "nan"){
+                        lb_display.text = String(0)
+                    }
+                        //            if(lb_display.text == "nan"){
+                        //                lb_display.text = String(0)
+                        //                //                numberScreen = Double(lb_display.text!)!
+                        //            }
+                        
+                    else{
+                        lb_display.text = lb_display.text! + String(sender.tag - 1)
+                        numberScreen = Double(lb_display.text!)!
+                    }
+                    
+                }
+                catch{
+                    lb_display.text = String(0)
+                }
+                
+                
+                
+            }
+
         }
         
     }
     
+    @IBAction func btnPhanTram(_ sender: Any) {
+        if(lb_display.text != ""){
+//            if(afterequal){
+//                
+//            }
+            NSLog(String("Previous:" + String(previousnumber)))
+            NSLog(String(previousnumber/100))
+            
+            lb_display.text = String(forZero(temp: Double(lb_display.text!)!/100))
+                //String(Double(lb_display.text)/100);
+        }
+
+    }
+//    @IBAction func btnPhanTram(_ sender: Any) {
+//            }
     @IBAction func Calculate(_ sender: UIButton) {
+        // 11. = AC , 12 = Chia, 13 = Nhan , 14 = Tru, 15 = cong, 16 = , 17 = +/- , 18 = %
+        //if(sender.tag ==)
         if sender.tag != 11 && sender.tag != 16 && lb_display.text != "" {
+            
+            //=====
             if sender.tag == 12 { //Chia
                 //
                 if afterequal == false {
@@ -71,6 +126,11 @@ class ViewController: UIViewController {
                         numberScreen = Double(lb_display.text!)!
                         lb_display.text = String(forZero(temp: previousnumber+numberScreen))
                     }
+                    else if operation == 18 && performingMath == false{ //%
+                        numberScreen = 100
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                        NSLog("% :" + String(previousnumber/numberScreen))
+                                            }
                 }
                 else {
                     afterequal = false
@@ -78,6 +138,7 @@ class ViewController: UIViewController {
                 
                 //
             }
+                //======
             else if sender.tag == 13 { //Nhan
                 //
                 if afterequal == false {
@@ -99,6 +160,11 @@ class ViewController: UIViewController {
                         numberScreen = Double(lb_display.text!)!
                         lb_display.text = String(forZero(temp: previousnumber+numberScreen))
                     }
+                    else if operation == 18 && performingMath == false{ //%
+                        numberScreen = 100
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                        NSLog("% :" + String(previousnumber/numberScreen))
+                    }
                 }
                 else {
                     afterequal = false
@@ -106,7 +172,8 @@ class ViewController: UIViewController {
                 
                 //
             }
-            else if sender.tag == 14 {//Tru
+            //=======
+            if sender.tag == 12 { //Chia
                 //
                 if afterequal == false {
                     if operation == 12 && performingMath == false{ //Chia
@@ -126,6 +193,46 @@ class ViewController: UIViewController {
                     else if operation == 15 && performingMath == false{ //Cong
                         numberScreen = Double(lb_display.text!)!
                         lb_display.text = String(forZero(temp: previousnumber+numberScreen))
+                    }
+                    else if operation == 18 && performingMath == false{ //%
+                        numberScreen = 100
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                        NSLog("% :" + String(previousnumber/numberScreen))
+                    }
+                }
+                else {
+                    afterequal = false
+                }
+                
+                //
+            }
+                //=====
+            else if sender.tag == 18 {//%
+                //
+                lb_display.text = String(previousnumber/100);
+                if afterequal == false {
+                    if operation == 12 && performingMath == false{ //Chia
+                        numberScreen = Double(lb_display.text!)!
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                    }
+                    else if operation == 13 && performingMath == false{ //Nhan
+                        numberScreen = Double(lb_display.text!)!
+                        lb_display.text = String(forZero(temp: previousnumber*numberScreen))
+                        
+                    }
+                    else if operation == 14 && performingMath == false{ //Tru
+                        numberScreen = Double(lb_display.text!)!
+                        lb_display.text = String(forZero(temp: previousnumber-numberScreen))
+                        
+                    }
+                    else if operation == 15 && performingMath == false{ //Cong
+                        numberScreen = Double(lb_display.text!)!
+                        lb_display.text = String(forZero(temp: previousnumber+numberScreen))
+                    }
+                    else if operation == 18 && performingMath == false { //Cong
+                        numberScreen = 100
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                        NSLog("% :" + String(previousnumber/numberScreen))
                     }
                 }
                 else {
@@ -154,6 +261,12 @@ class ViewController: UIViewController {
                         numberScreen = Double(lb_display.text!)!
                         lb_display.text = String(forZero(temp: previousnumber+numberScreen))
                     }
+                    
+                    else if operation == 18 && performingMath == false{ //%
+                        numberScreen = 100
+                        lb_display.text = String(forZero(temp: previousnumber/numberScreen))
+                        NSLog("% :" + String(previousnumber/numberScreen))
+                    }
                 }
                 else {
                     afterequal = false
@@ -164,7 +277,7 @@ class ViewController: UIViewController {
             performingMath = true
         }
         else if sender.tag == 11 {
-            lb_display.text = ""
+            lb_display.text = String(0)
             previousnumber = 0
             numberScreen = 0
             operation = 0
@@ -201,6 +314,7 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        lb_display.text = String(0)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
